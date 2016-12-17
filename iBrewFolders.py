9e03671320a6b298c6410bff081be446948b32d6
Tristan Crispijn <Tristan@MacBook-Pro.local>
2016-12-17 15:17:09 +0100

    .

--- iBrewFolders.py
+++ iBrewFolders.py
@@ -72,7 +72,8 @@ class AppFolders():
             if platform.system() == "Windows":
                 folder = os.path.join( AppFolders.windowsAppDataFolder(), appname )
             else:
-                folder = "/var/log/"+appname
+                folder = "/var/log"
+                #folder = "/var/log/"+appname
         else:
             folder = os.path.join(AppFolders.userFolder(), "logs")
         return folder
@@ -85,7 +86,8 @@ class AppFolders():
             elif platform.system() == "Darwin":
                 folder = os.path.join( '/Library/Application Support/'+appname)
             else:
-                folder = "/etc/"+appname
+                folder = "/etc"
+                #folder = "/etc/"+appname
         else:
             folder = os.path.join(AppFolders.userFolder())
         return folder
